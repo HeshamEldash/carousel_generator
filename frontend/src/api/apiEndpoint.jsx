7e21944838d55d09a1565d872c304ef18a28b45e
HeshamEldash <heshamaldash44@gmail.com>
2023-06-11 15:13:54 +0100

    added apiend point variable

--- frontend/src/api/apiEndpoint.jsx
+++ frontend/src/api/apiEndpoint.jsx
@@ -1,1 +1,9 @@
-export default "http://127.0.0.1:5000/"
+let api;
+
+if (import.meta.env.apiEndpointEnv) {
+  api = import.meta.env.VITE_API_URL;
+} else {
+  api = "http://127.0.0.1:5000/";
+}
+
+export default api;
