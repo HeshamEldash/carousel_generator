66f2271b8f4ab3f0341009f39f8deda717a0a382
HeshamEldash <heshamaldash44@gmail.com>
2023-06-11 15:23:51 +0100

    added apiend point variable4

--- frontend/src/api/apiEndpoint.jsx
+++ frontend/src/api/apiEndpoint.jsx
@@ -1,6 +1,4 @@
-const api = import.meta.env.API_URL || "http://127.0.0.1:5000/";
-
-console.log(import.meta.env.API_URL)
-console.log(import.meta.env)
+// const api = import.meta.env.API_URL || "http://127.0.0.1:5000/";
+const api = "https://backend-testingcodeinfo.b4a.run/"
 
 export default api;
